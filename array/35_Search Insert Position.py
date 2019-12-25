d54ecb2bf8e3fa14799d155c2d271bf5c5c3d43c
songfish <songyu_727@163.com>
2019-12-25 15:28:40 +0800

    add solution

---
+++ array/35_Search Insert Position.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        left_pointer = 0
+        right_pointer = len(nums) - 1
+        if nums[right_pointer] < target:
+            return right_pointer + 1
+        if nums[left_pointer] > target:
+            return left_pointer
+        if nums[right_pointer] == target:
+            return right_pointer
+        if nums[left_pointer] == target:
+            return left_pointer
+        while left_pointer < right_pointer - 1:
+            mid = (left_pointer + right_pointer) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                right_pointer = mid
+            if nums[mid] < target:
+                left_pointer = mid
+        return left_pointer + 1
+
+S = Solution()
+print(S.searchInsert(nums=[1,3],target=1))
+
+"""
+2019.12.25反思：
+    我这是暴力二分解法？
+"""
